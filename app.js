adf70fc89df67fab0d9c6f6fb8478c039ed63bee
Amruth K Ciby <amruthkciby@gmail.com>
2026-06-27 22:29:55 +0530

    push

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ import { Team } from './models/Team.js';
 
 const { team1, team2 } = selectTeams();
 
-console.log('Team 1:', team1);
-console.log('Team 2:', team2);
+console.log('Team 1:', RCB);
+console.log('Team 2:', CSK);
